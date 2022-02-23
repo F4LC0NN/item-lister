19d5547d164aa58d2a2def4a89c42db3cc1cf5fb
F4LC0NN <thomasruben25@gmail.com>
2022-02-23 13:04:25 +0100

    Create an event to submit new items in the list

--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@ SELECT ELEMENTS
 const list = document.querySelector('#list-group');
 const input = document.querySelector('#add-item-bar');
 const submitButton = document.querySelector('#submit-button');
+
+/*
+------------------------------------------------
+CREATE ITEMS
+------------------------------------------------
+*/
+
+// Create an event to add item to list by clicking on the submit button
+submitButton.addEventListener('click', addRemoveItems);
