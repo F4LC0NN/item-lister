574c17185368157e3eaf093a28ef1059ea7082a8
F4LC0NN <thomasruben25@gmail.com>
2022-02-23 13:01:27 +0100

    Create selectors to target the list, input & submit button

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*
+------------------------------------------------
+SELECT ELEMENTS
+------------------------------------------------
+*/
+
+const list = document.querySelector('#list-group');
+const input = document.querySelector('#add-item-bar');
+const submitButton = document.querySelector('#submit-button');
