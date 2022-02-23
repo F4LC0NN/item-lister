b8d2086385583459b2c9e7395a86843dc931e913
F4LC0NN <thomasruben25@gmail.com>
2022-02-23 13:05:57 +0100

    Create a function to add new items

--- js/script.js
+++ js/script.js
@@ -16,3 +16,22 @@ CREATE ITEMS
 
 // Create an event to add item to list by clicking on the submit button
 submitButton.addEventListener('click', addRemoveItems);
+
+// Create a function to add items
+function addRemoveItems(event) {
+  // Prevent the items to disappear whenever the submit button is clicked
+  event.preventDefault();
+
+  // Check if the input value is empty
+  // If the input value is empty, deactivate the submit button
+  let inputValue = input.value;
+  if (inputValue === '') {
+    return;
+  }
+
+  // Create an li element called item
+  const item = document.createElement('li');
+  item.className = "list-item";
+  item.textContent = input.value;
+  list.appendChild(item);
+}
